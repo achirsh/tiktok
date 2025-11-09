b86128e621d3222d75835d532dbf90aa3827e540
achirsh <qbachirsh@outlook.com>
2025-11-09 22:13:31 +0800

    no message

--- src/config/menu.jsx
+++ src/config/menu.jsx
@@ -1,10 +1,14 @@
 import { lazy } from 'react';
 
 const TermsOfService = lazy(() => import('../pages/TermsOfService'));
-
+const Home = lazy(() => import('../pages/home'));
 
 // 初始化路由
 const allMenu = [
+    {
+        page: '/',
+        component: <Home />
+    },
     { 
         page: '/termsOfService',
         component: <TermsOfService /> 
